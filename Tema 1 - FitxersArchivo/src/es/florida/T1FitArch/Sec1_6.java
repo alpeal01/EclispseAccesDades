10375186aa3368ae7501ba79b387dc480fa1f2af
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-09-19 12:22:24 +0200

    modif del ej 6

--- Tema 1 - FitxersArchivo/src/es/florida/T1FitArch/Sec1_6.java
+++ Tema 1 - FitxersArchivo/src/es/florida/T1FitArch/Sec1_6.java
@@ -1,42 +1,33 @@
-package es.florida.T1FitArch;
-import java.io.File;
 
-//Modifica el programa anterior per que tinga en compte que si no se li passa ninguna extensió
-//com a paràmetre, mostre tot el contingut del directori.
+ package es.florida.T1FitArch;
+import java.io.File;
+import java.io.FilenameFilter;
 
 public class Sec1_6 {
-
+//Modifica el programa anterior per que tinga en compte que si no se li passa ninguna extensió
+//com a paràmetre, mostre tot el contingut del directori.
 	public static void main(String[] args) {
 		
 		File file = new File(args[0]);
-		
-		
-		
-		
-			
-			String [] lista  = file.list();
-			
-			
-			if(args[1] == null ) {
-				for (String data : lista) {
-					
-					System.out.println(data);
-				}
-				
-			}else {
-			
+		String lista[];
+		try {
+		 lista = file.list(new FilenameFilter() {
+		    @Override
+		    public boolean accept(File dir, String name) {
+		        return name.endsWith(args[1]);
+		    }
+		});}catch (Exception e) {
+			System.out.println(e.getMessage());
+			 lista = file.list();
+		}
+		System.out.println("Resultados: "+ lista.length);
 			for (String data : lista) {
 				
-				if(data.contains(args[1])){
 					
+				
 					System.out.println(data);
 					
 				}
-				
-			}}
-			
-		
 		
 	}
-
 }
