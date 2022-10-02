89f57cb0d2f1847aa01fb799e5d2d7e19e632981
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-10-02 16:30:19 +0200

    retoques miniproyecto 2

--- Tema 1 - FitxersArchivo/src/es/florida/T1FitArch/Sec2_9/Vista.java
+++ Tema 1 - FitxersArchivo/src/es/florida/T1FitArch/Sec2_9/Vista.java
@@ -1,30 +1,55 @@
 package es.florida.T1FitArch.Sec2_9;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
+import javax.swing.JTextArea;
 
 public class Vista extends JFrame {
 
-	 JPanel contentPane;
-	 JTextField txtMostrar;
-	 JTextField txtBuscar;
-	 JTextField txtReemplazar;
-	 JTextField txtResultant;
-	 JTextField txtDir;
-	 JButton btnCargar;
-	 
-	  public JButton getBtnCargar() { return btnCargar; }
-	  public JTextField getTxtMostrar() {return txtMostrar;}
+	JPanel contentPane;
+	JTextField txtBuscar;
+	JTextField txtReemplazar;
+	JTextField txtDir;
+	JButton btnCargar;
+	JTextArea txtArMostrar;
+	JTextArea txtArRem;
+	JButton btnRemplazar;
+	JButton btnBuscar;
 
-	  	
+	public JButton getBtnCargar() {
+		return btnCargar;
+	}
+
+	public JTextField gettxtDir() {
+		return txtDir;
+	};
+
+	public JTextArea getTxtArMostrar() {
+		return txtArMostrar;
+	};
+	
+	public JTextArea getTxtArRem() {
+		return txtArRem;
+	};
+	
+	public JButton getBtnRemplazar() {
+		return btnRemplazar;
+	}
+	public JTextField getTxtReemplazar() {
+		return txtReemplazar;
+	}
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+	
+	public JTextField getTxtBuscar() {
+		return txtBuscar;
+	}
+	
+
 	/**
 	 * Launch the application.
 	 */
@@ -40,46 +65,44 @@ public class Vista extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		txtMostrar = new JTextField();
-		txtMostrar.setBounds(90, 10, 554, 161);
-		contentPane.add(txtMostrar);
-		txtMostrar.setColumns(10);
-		
+
 		txtBuscar = new JTextField();
 		txtBuscar.setColumns(10);
 		txtBuscar.setBounds(96, 202, 125, 19);
 		contentPane.add(txtBuscar);
-		
-		JButton btnBuscar = new JButton("Buscar");
+
+		btnBuscar = new JButton("Buscar");
 		btnBuscar.setBounds(231, 201, 85, 21);
 		contentPane.add(btnBuscar);
-		
+
 		txtReemplazar = new JTextField();
 		txtReemplazar.setColumns(10);
 		txtReemplazar.setBounds(390, 202, 141, 19);
 		contentPane.add(txtReemplazar);
-		
-		JButton btnRemplazar = new JButton("Reemplazar");
+
+		btnRemplazar = new JButton("Reemplazar");
 		btnRemplazar.setBounds(541, 201, 103, 21);
 		contentPane.add(btnRemplazar);
-		
-		txtResultant = new JTextField();
-		txtResultant.setEditable(false);
-		txtResultant.setColumns(10);
-		txtResultant.setBounds(90, 231, 554, 161);
-		contentPane.add(txtResultant);
-		
+
 		txtDir = new JTextField();
 		txtDir.setColumns(10);
-		txtDir.setBounds(119, 419, 125, 19);
+		txtDir.setBounds(96, 418, 125, 19);
 		contentPane.add(txtDir);
-		
-		btnCargar = new JButton("Cargar Archivo");
-		
 
-		btnCargar.setBounds(254, 418, 125, 21);
+		btnCargar = new JButton("Cargar Archivo");
+
+		btnCargar.setBounds(231, 418, 125, 21);
 		contentPane.add(btnCargar);
+
+		txtArMostrar = new JTextArea();
+		txtArMostrar.setEditable(false);
+		txtArMostrar.setBounds(96, 11, 548, 180);
+		contentPane.add(txtArMostrar);
+		
+		txtArRem = new JTextArea();
+		txtArRem.setEditable(false);
+		txtArRem.setBounds(96, 232, 548, 180);
+		contentPane.add(txtArRem);
 		this.setVisible(true);
 	}
 }
