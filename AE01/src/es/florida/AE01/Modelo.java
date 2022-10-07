ff04dd127cddafdc55668912ffe9fe3547cb665a
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-10-07 19:03:10 +0200

    que aproveche

--- AE01/src/es/florida/AE01/Modelo.java
+++ AE01/src/es/florida/AE01/Modelo.java
@@ -14,10 +14,10 @@ public class Modelo {
 		
 		
 		if(dFile.isFile()) {
-			String[] fileName = (dFile.getName()).split(".");
+	
 			
-			 return "ARCHIVO"+"\nNombre: "+ fileName[0]+
-					 "\nExtensión: " +fileName[1] +
+			 return "ARCHIVO"+"\nNombre: "+ dFile.getName()+
+					 "\nExtensión: " +dFile.getName() +
 					 "\nTamaño: "+dFile.getTotalSpace()+
 					 "\nFecha creación: "+dFile.lastModified()+
 					 "\nUbicación: "+dFile.getAbsolutePath();
