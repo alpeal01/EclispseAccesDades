d76c1ba7d122366b1859c5cf8b1183f5f74b4b07
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-10-11 12:36:15 +0200

    funcion buqueda de palabras

--- AE01/src/es/florida/AE01/Controlador.java
+++ AE01/src/es/florida/AE01/Controlador.java
@@ -1,7 +1,10 @@
 package es.florida.AE01;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JOptionPane;
 
@@ -31,11 +34,68 @@ public class Controlador {
 			}
 
 		});
+		
+//		vista.getList().addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				
+//				String texto = vista.getTxtBusq().getText(); // ./
+//				//List<String> listaBusqueda = new ArrayList<String>();
+//				String s = (String) vista.getList().getSelectedValue(); // .classpath
+//				if (texto.equals(texto + s + "/")) { // ./.classpath/ == ./.classpath/.classpath/
+//
+//				} else {
+//					texto = vista.getTxtBusq().getText() + s + "/";
+//					System.out.println(texto);
+//					vista.getTxtBusq().setText(texto);
+//				}
+//
+//			}
+//		});
 
+		vista.getBtnEncontrar().addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				
+
+				String [] conetnt = (modelo.cargaContendio(vista.getTxtBusq().getText())).split(" ");
+				String textFinal="";
+				String busqWord = vista.getTxtEncontrar().getText();
+				//<H1><FONT COLOR=white>The text is white</H1>
+				for (String word : conetnt) {
+					
+					if(word.equals(busqWord)) {
+						
+						textFinal+= "<H1>"+word+"</H1>";
+						textFinal+=" ";
+						
+					}else {
+						textFinal+=word;
+						textFinal+=" ";
+					}
+					
+				}
+				
+				
+				vista.getTxtArMost().setText(textFinal);
+				
+				
+				
+
+			}
+
+		});
+		
+		
+		
 	}
 
 	/*
 	 * public void iniciarVista() { vista = new Vista(); vista.setVisible(true); }
 	 */
+	
+	
+	
 
 }
--- AE01/src/es/florida/AE01/Vista.java
+++ AE01/src/es/florida/AE01/Vista.java
@@ -13,16 +13,19 @@ import javax.swing.JTextArea;
 import javax.swing.JList;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JEditorPane;
 
 public class Vista extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtBusq;
-	JTextArea txtArMost;
 	JButton btnBusq;
 	JList<String> list;
+	private JTextField txtEncontrar;
+	JButton btnEncontrar ;
+	private JEditorPane txtArMost;
 
-	public JTextArea getTxtArMost() {
+	public JEditorPane getTxtArMost() {
 		return this.txtArMost;
 	}
 
@@ -37,10 +40,21 @@ public class Vista extends JFrame {
 	public JList<String> getList() {
 		return this.list;
 	}
+	
+	public JButton getBtnEncontrar () {
+		
+		return this.btnEncontrar;
+		
+	}
+	
+	public  JTextField getTxtEncontrar() {
+		return this.txtEncontrar;
+	}
 
 	public void setList(ArrayList<String> listFich) {
 		this.list = new JList<String>(listFich.toArray(new String[listFich.size()]));
 	}
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,7 +77,7 @@ public class Vista extends JFrame {
 	public Vista() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 617, 392);
+		setBounds(100, 100, 617, 421);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -78,11 +92,6 @@ public class Vista extends JFrame {
 		btnBusq = new JButton("Buscar");
 		btnBusq.setBounds(508, 4, 85, 21);
 		contentPane.add(btnBusq);
-
-		txtArMost = new JTextArea();
-		txtArMost.setEditable(false);
-		txtArMost.setBounds(162, 34, 431, 225);
-		contentPane.add(txtArMost);
 
 		list = new JList<String>();
 		list.addMouseListener(new MouseAdapter() {
@@ -105,8 +114,21 @@ public class Vista extends JFrame {
 		});
 		list.setBounds(10, 34, 142, 225);
 		contentPane.add(list);
+		
+		txtEncontrar = new JTextField();
+		txtEncontrar.setBounds(297, 270, 142, 20);
+		contentPane.add(txtEncontrar);
+		txtEncontrar.setColumns(10);
+		
+		btnEncontrar = new JButton("Encontrar palabra");
+		btnEncontrar.setBounds(449, 269, 139, 23);
+		contentPane.add(btnEncontrar);
+		
+		txtArMost = new JEditorPane();
+		txtArMost.setBounds(162, 39, 429, 220);
+		contentPane.add(txtArMost);
+		txtArMost.setContentType("text/html");
 
 		this.setVisible(true);
 	}
-
 }
