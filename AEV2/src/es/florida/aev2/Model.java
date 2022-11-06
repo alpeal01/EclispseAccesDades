ae12fdc6d0a6b743fc9cb224b0043e4efb1133ea
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-11-06 18:52:01 +0100

    conexiones

--- AEV2/src/es/florida/aev2/Model.java
+++ AEV2/src/es/florida/aev2/Model.java
@@ -1,6 +1,11 @@
 package es.florida.aev2;
 
+
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
@@ -10,6 +15,8 @@ public class Model {
 	String conexio;
 	String user;
 	String pswd;
+	Statement stmt;
+	Connection con;
 	
 	
 	public Model() {
@@ -25,8 +32,6 @@ public class Model {
 		this.user = eElement.getElementsByTagName("user").item(0).getTextContent();
 		this.pswd = eElement.getElementsByTagName("pswd").item(0).getTextContent();
 		
-		
-		
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -35,7 +40,42 @@ public class Model {
 		
 	}
 	
+	public String dbConnect() {
+		try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		this.con = DriverManager.getConnection(this.conexio,this.user,this.pswd);
+		this.stmt = con.createStatement();
+		return "Conexión establecida";
+		
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+		return "Fallo al conectar";
+		
+	}
 	
+	public String dbDisconnect() {
+		
+		try {
+			this.stmt.close();
+			this.con.close();
+			return "Conexion cerrada";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "Fallo al cerrar conexion";
+	}
+	
+	public void dbStruc () {
+		
+		
+		
+	}
+	
+	//describir tablas con DESCRIBE [nombre de la tabla];
 	
 
 
