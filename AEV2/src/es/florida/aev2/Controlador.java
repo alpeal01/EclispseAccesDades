3bbcf4f0bfc0167c594ba95669b743bca319ffb3
HectorSanchezLuque <hesalu@floridauniversitaria.es>
2022-11-06 19:01:45 +0100

    comit

--- AEV2/src/es/florida/aev2/Controlador.java
+++ AEV2/src/es/florida/aev2/Controlador.java
@@ -2,24 +2,31 @@ package es.florida.aev2;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 public class Controlador {
 	private Vista vista;
 	private Model modelo;
 
 	// El constructor rep instancia de la vista i del Model
-	
+
 	public Controlador(Vista vista, Model modelo) {
 		this.vista = vista;
 		this.modelo = modelo;
 		initEventHandlers();
+
 	}
+
 	public void initEventHandlers() {
 //		vista."NomdeLboto".addActionListener(new ActionListener() {
 //		public void actionPerformed(ActionEvent arg0) {
 //		 //Codi que s’executa en pulsar el botó acceptar
 //		}
 //		});
-		//Afegir tants gestors com acción vulga realitzar
-	
-		}
+		// Afegir tants gestors com acción vulga realitzar
+		modelo.controlLogin();
+	}
 }
--- AEV2/src/es/florida/aev2/Vista.java
+++ AEV2/src/es/florida/aev2/Vista.java
@@ -5,10 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class Vista extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtQuery;
 
 	/**
 	 * Launch the application.
@@ -31,21 +38,44 @@ public class Vista extends JFrame {
 	 */
 	public Vista() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtQuery = new JTextField();
+		txtQuery.setBounds(203, 20, 437, 27);
+		contentPane.add(txtQuery);
+		txtQuery.setColumns(10);
+		
+		JLabel lblConsultas = new JLabel("Consultes: ");
+		lblConsultas.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblConsultas.setBounds(81, 26, 99, 21);
+		contentPane.add(lblConsultas);
+		
+		JButton txtExecutar = new JButton("Executar");
+		txtExecutar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtExecutar.setBounds(664, 21, 111, 31);
+		contentPane.add(txtExecutar);
+		
+		JTextArea txtResultats = new JTextArea();
+		txtResultats.setBounds(49, 80, 771, 305);
+		contentPane.add(txtResultats);
+		
+		JButton btnTancar = new JButton("Tancar conexió");
+		btnTancar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnTancar.setBounds(622, 413, 185, 38);
+		contentPane.add(btnTancar);
 		initComponents();
 	}
 	
 	public void initComponents() {
 		setResizable(false);
-		setTitle("Aplicacio");
+		setTitle("Aplicació");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		this.setVisible(true);
 		}
-
-
 }
