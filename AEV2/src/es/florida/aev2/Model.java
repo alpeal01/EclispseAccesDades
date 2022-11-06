6be3c69dc25d5fd01c066ac99b45174f796df2f5
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-11-06 18:29:00 +0100

    xml

--- AEV2/src/es/florida/aev2/Model.java
+++ AEV2/src/es/florida/aev2/Model.java
@@ -1,10 +1,42 @@
 package es.florida.aev2;
 
+import java.io.File;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+
 public class Model {
+	String conexio;
+	String user;
+	String pswd;
 	
 	
 	public Model() {
+		super();
+		try {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document document = dBuilder.parse(new File("connectDB.xml"));
+		NodeList nodeList = document.getElementsByTagName("conexio"); 
+		Node node = nodeList.item(0);
+		Element eElement = (Element) node;
+		this.conexio = eElement.getElementsByTagName("url").item(0).getTextContent();
+		this.user = eElement.getElementsByTagName("user").item(0).getTextContent();
+		this.pswd = eElement.getElementsByTagName("pswd").item(0).getTextContent();
+		
+		
+		
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
+	
+	
+	
+
 
 }
