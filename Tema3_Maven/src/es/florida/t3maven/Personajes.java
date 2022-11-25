0ac71d9fee51cc9b0e58420ef7d83c932324e86c
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-11-25 13:20:47 +0100

    datos

--- Tema3_Maven/src/es/florida/t3maven/Personajes.java
+++ Tema3_Maven/src/es/florida/t3maven/Personajes.java
@@ -1,12 +1,12 @@
 package es.florida.t3maven;
 
-import javax.imageio.spi.ServiceRegistry;
+import java.io.Serializable;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-
-import com.mysql.cj.xdevapi.SessionFactory;
+import org.hibernate.service.ServiceRegistry;
 
 public class Personajes {
 	
@@ -18,25 +18,31 @@ public class Personajes {
 	
 	int def;
 	
-	public Personajes(int id, String name, int atq, int def) {
+	public Personajes(String name, int atq, int def) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.atq = atq;
 		this.def = def;
 	}
 
 	static void session() {
-		
+		// Carrega la configuracio i crea un session factory
 		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
 		configuration.addClass(Personajes.class);
 		ServiceRegistry registry = new
 		StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
 		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+		
 		// Obri una nova sessió de la session factory
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		
 		//Ací les operacio/ns CRUD (crear, llegir, actualitzar, borrar)
+		Personajes per = new Personajes("Casio", 5,10 );
+		Serializable id = (Serializable) session.save(per);
+		System.out.println(id);
+		
+		
 		//Commit de la transacció i tanca de sessió
 		session.getTransaction().commit();
 		session.close();
@@ -48,6 +54,7 @@ public class Personajes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		session();
 
 	}
 
