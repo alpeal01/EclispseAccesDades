cb39c1be80305baef84c8e7b4db7be2343210324
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-11-28 11:45:06 +0100

    ajersisao de maven

--- Tema3_Maven/src/es/florida/t3maven/Personajes.java
+++ Tema3_Maven/src/es/florida/t3maven/Personajes.java
@@ -9,52 +9,165 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class Personajes {
-	
+
 	int id;
 	
-	String name;
+	String nombre;
+
+	int ataque;
+
+	int defensa;
+
+
 	
-	int atq;
-	
-	int def;
-	
-	public Personajes(String name, int atq, int def) {
+	public Personajes(int id, String name, int atq, int def) {
 		super();
-		this.name = name;
-		this.atq = atq;
-		this.def = def;
+		this.id = id;
+		this.nombre = name;
+		this.ataque = atq;
+		this.defensa = def;
+	}
+	
+	public Personajes() {}
+
+	public void setId(int value) {
+		this.id = value;
 	}
 
-	static void session() {
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+	static void insertar(Personajes per) {
+		// Carrega la configuracio i crea un session factory
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		configuration.addClass(Personajes.class);
+		ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+				.build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+
+		// Obri una nova sessió de la session factory
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		// Ací les operacio/ns CRUD (crear, llegir, actualitzar, borrar)
+		 
+		Serializable id = (Serializable) session.save(per);
+		System.out.println(id);
+
+		// Commit de la transacció i tanca de sessió
+		session.getTransaction().commit();
+		session.close();
+
+	}
+	
+	static Personajes leer (int id) {
 		// Carrega la configuracio i crea un session factory
 		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
 		configuration.addClass(Personajes.class);
 		ServiceRegistry registry = new
 		StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
 		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
-		
 		// Obri una nova sessió de la session factory
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		
 		//Ací les operacio/ns CRUD (crear, llegir, actualitzar, borrar)
-		Personajes per = new Personajes("Casio", 5,10 );
-		Serializable id = (Serializable) session.save(per);
-		System.out.println(id);
+		Personajes per = (Personajes) session.get(Personajes.class, id);
 		
 		
 		//Commit de la transacció i tanca de sessió
 		session.getTransaction().commit();
 		session.close();
-
 		
+		return per;
 		
 		
 	}
+	
+	static void modificarNombre(int id, String newName) {
+		
+		// Carrega la configuracio i crea un session factory
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		configuration.addClass(Personajes.class);
+		ServiceRegistry registry = new
+		StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+		// Obri una nova sessió de la session factory
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		//Ací les operacio/ns CRUD (crear, llegir, actualitzar, borrar)
+		Personajes pers = (Personajes) session.load(Personajes.class, id);
+		pers.setNombre(newName);
+		session.update(pers);
+		System.out.println("Nombre cambiado con exito");
+		
+		//Commit de la transacció i tanca de sessió
+		session.getTransaction().commit();
+		session.close();
+		
+		
+	}
+	static void eliminar(int id) {
+		// Carrega la configuracio i crea un session factory
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		configuration.addClass(Personajes.class);
+		ServiceRegistry registry = new
+		StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+		// Obri una nova sessió de la session factory
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		//Ací les operacio/ns CRUD (crear, llegir, actualitzar, borrar)
+		
+		Personajes cancion = new Personajes();
+		cancion.setId(id);
+		session.delete(cancion);
+		System.out.println("Personaje eliminado");
+		
+		
+		//Commit de la transacció i tanca de sessió
+		session.getTransaction().commit();
+		session.close();
+		
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		session();
+//		insertar(new Personajes(0,"Atelio", 10, 2));
+//		Personajes per = leer(1);		
+//		System.out.println(per.nombre);
+//		modificarNombre(3,"Kasani");
+		eliminar(2);
+		
+		
+
 
 	}
 
